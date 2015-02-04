53558e4f46d6137ad69189a310f965ef51d4773a
rrossinvicara <richard.ross@invicara.com>
2015-02-04 15:09:59 -0500

    Did some groundwork for EMOB-212

---
+++ INVBIMAssure/Source/View Controllers/Account/INVUserProfileTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  INVUserProfileTableViewController.h
+//  INVBIMAssure
+//
+//  Created by Richard Ross on 2/4/15.
+//  Copyright (c) 2015 Invicara Inc. All rights reserved.
+//
+
+#import "INVCustomTableViewController.h"
+
+@interface INVUserProfileTableViewController : INVCustomTableViewController
+
+@end
---
+++ INVBIMAssure/Source/View Controllers/Account/INVUserProfileTableViewController.m
@@ -0,0 +1,28 @@
+//
+//  INVUserProfileTableViewController.m
+//  INVBIMAssure
+//
+//  Created by Richard Ross on 2/4/15.
+//  Copyright (c) 2015 Invicara Inc. All rights reserved.
+//
+
+#import "INVUserProfileTableViewController.h"
+
+@interface INVUserProfileTableViewController ()
+
+@property IBOutlet UITextField *emailTextField;
+@property IBOutlet UITextField *firstNameTextField;
+@property IBOutlet UITextField *lastNameTextField;
+
+@property IBOutlet UITextField *addressTextField;
+@property IBOutlet UITextField *phoneNumberTextField;
+@property IBOutlet UITextField *companyTextField;
+
+@end
+
+@implementation INVUserProfileTableViewController
+
+- (void)fetchUserProfileDetails
+{
+}
+@end
