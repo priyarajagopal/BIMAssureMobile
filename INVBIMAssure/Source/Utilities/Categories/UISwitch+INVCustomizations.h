c966003cfae9208b1276da08b7d92df3f3e151de
rrossinvicara <richard.ross@invicara.com>
2015-02-19 09:29:07 -0500

    Fixed switch event handling bug.

---
+++ INVBIMAssure/Source/Utilities/Categories/UISwitch+INVCustomizations.h
@@ -0,0 +1,13 @@
+//
+//  UISwitch+INVCustomizations.h
+//  INVBIMAssure
+//
+//  Created by Richard Ross on 2/19/15.
+//  Copyright (c) 2015 Invicara Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UISwitch (INVCustomizations)
+
+@end
---
+++ INVBIMAssure/Source/Utilities/Categories/UISwitch+INVCustomizations.m
@@ -0,0 +1,44 @@
+//
+//  UISwitch+INVCustomizations.m
+//  INVBIMAssure
+//
+//  Created by Richard Ross on 2/19/15.
+//  Copyright (c) 2015 Invicara Inc. All rights reserved.
+//
+
+#import "UISwitch+INVCustomizations.h"
+#import "INVRuntimeUtils.h"
+
+@import ObjectiveC.runtime;
+
+static void (*oldSendAction)(id, SEL, SEL, id, UIEvent *);
+
+@implementation UISwitch (INVCustomizations)
+
+- (BOOL)_inv_previouslyOn
+{
+    static const void *previouslyOnKey = &previouslyOnKey;
+    id result = objc_getAssociatedObject(self, previouslyOnKey);
+    objc_setAssociatedObject(self, previouslyOnKey, @([self isOn]), OBJC_ASSOCIATION_RETAIN);
+
+    return [result boolValue];
+}
+
+- (void)_inv_sendAction:(SEL)action to:(id)target forEvent:(UIEvent *)event
+{
+    NSArray *actions = [self actionsForTarget:target forControlEvent:UIControlEventValueChanged];
+    if ([self _inv_previouslyOn] == [self isOn] && [actions containsObject:NSStringFromSelector(action)]) {
+        return;
+    }
+
+    oldSendAction(self, _cmd, action, target, event);
+}
+
+@end
+
+__attribute__((constructor)) static void UISwitch_INVCustomizations_Init()
+{
+    Class kls = [UISwitch class];
+
+    oldSendAction = (void *) safeSwapMethods(kls, @selector(sendAction:to:forEvent:), @selector(_inv_sendAction:to:forEvent:));
+}
