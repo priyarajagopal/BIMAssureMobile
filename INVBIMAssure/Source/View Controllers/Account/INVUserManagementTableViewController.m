7091b185fccda1481531a2a6530b3c573fdfb0b4
rrossinvicara <richard.ross@invicara.com>
2015-03-11 12:59:35 -0400

    Added disable account support.

--- INVBIMAssure/Source/View Controllers/Account/INVUserManagementTableViewController.m
+++ INVBIMAssure/Source/View Controllers/Account/INVUserManagementTableViewController.m
@@ -14,6 +14,7 @@
 #define ROW_PENDING_INVITES 1
 #define ROW_INVITE_USERS 2
 #define ROW_EDIT_ACCOUNT 3
+#define ROW_DISABLE_ACCOUNT 4
 
 @interface INVUserManagementTableViewController ()
 
@@ -60,6 +61,67 @@
     if (indexPath.row == ROW_EDIT_ACCOUNT) {
         [self.editAccountTransition perform:nil];
     }
+
+    if (indexPath.row == ROW_DISABLE_ACCOUNT) {
+        UIAlertController *alertController =
+            [UIAlertController alertControllerWithTitle:NSLocalizedString(@"DISABLE_ACCOUNT_TITLE", nil)
+                                                message:NSLocalizedString(@"DISABLE_ACCOUNT_MESSAGE", nil)
+                                         preferredStyle:UIAlertControllerStyleAlert];
+
+        [alertController addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"DISABLE_ACCOUNT_CANCEL_TITLE", nil)
+                                                            style:UIAlertActionStyleCancel
+                                                          handler:nil]];
+
+        [alertController
+            addAction:[UIAlertAction
+                          actionWithTitle:NSLocalizedString(@"DISABLE_ACCOUNT_PERFORM_TITLE", nil)
+                                    style:UIAlertActionStyleDestructive
+                                  handler:^(UIAlertAction *action) {
+                                      [self.globalDataManager.invServerClient
+                                          disableAccountForSignedInUserWithCompletionBlock:INV_COMPLETION_HANDLER {
+                                              INV_ALWAYS:
+                                              INV_SUCCESS : {
+                                                  [self.globalDataManager.invServerClient
+                                                      logOffSignedInAccountWithCompletionBlock:INV_COMPLETION_HANDLER {
+                                                          INV_ALWAYS:
+                                                          INV_SUCCESS:
+                                                              self.globalDataManager.loggedInAccount = nil;
+                                                              [self.globalDataManager deleteCurrentlySavedDefaultAccountFromKC];
+                                                              [self notifyAccountLogout];
+
+                                                          INV_ERROR:
+                                                              INVLogError(@"%@", error);
+                                                              [self presentDisableErrorMessage];
+                                                      }];
+                                              }
+
+                                              INV_ERROR:
+                                                  INVLogError(@"%@", error);
+                                                  [self presentDisableErrorMessage];
+                                          }];
+
+                                  }]];
+
+        [self presentViewController:alertController animated:YES completion:nil];
+    }
+}
+
+- (void)notifyAccountLogout
+{
+    [[NSNotificationCenter defaultCenter] postNotificationName:INV_NotificationAccountLogOutSuccess object:nil userInfo:nil];
+}
+
+- (void)presentDisableErrorMessage
+{
+    UIAlertController *errorController =
+        [UIAlertController alertControllerWithTitle:NSLocalizedString(@"ERROR_ACCOUNT_DISABLE_TITLE", nil)
+                                            message:NSLocalizedString(@"ERROR_ACCOUNT_DISABLE_MESSAGE", nil)
+                                     preferredStyle:UIAlertControllerStyleAlert];
+
+    [errorController
+        addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"OK", nil) style:UIAlertActionStyleDefault handler:nil]];
+
+    [self presentViewController:errorController animated:YES completion:nil];
 }
 
 - (INVAccount *)loggedInAccount
