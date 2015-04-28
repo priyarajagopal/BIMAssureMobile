7b61430bf4523e6efb27580819bf3025880c8a77
priyarajagopal <priya.rajagopal@invicara.com>
2015-04-28 12:05:39 -0400

    linked with latest EMM that has fix for EMOB-442

--- INVBIMAssure/Source/View Controllers/Account/INVLoginViewController.m
+++ INVBIMAssure/Source/View Controllers/Account/INVLoginViewController.m
@@ -159,8 +159,7 @@ NSString *const KVO_INVLoginSuccess = @"loginSuccess";
 - (void)loginToServerWithUser:(NSString *)user andPassword:(NSString *)password
 {
     [self showLoginProgress];
-    NSLog(@"%s. user  %@, password %@",__func__,user,password);
-    
+     
     [self.globalDataManager.invServerClient signInWithUserName:user
                                                    andPassword:password
                                            withCompletionBlock:^(INVEmpireMobileError *error) {
